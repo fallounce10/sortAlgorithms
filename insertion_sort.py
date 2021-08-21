a31f29b5a014867d204e867b9bd603e9edbaf768
fallounce10 <pinhal.silva96@gmail.com>
2021-08-21 13:30:38 +0100

    Insertion Sort

---
+++ insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort(a_list):
+	for i in range(1, len(a_list)):
+		value = a_list[i]
+		j = i - 1
+		while j >= 0 and value < a_list[j]:
+			a_list[j+1] = a_list[j]
+			j -= 1
+		a_list[j+1] = value
+
+	return a_list   
